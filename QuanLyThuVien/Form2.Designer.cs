43b1facbe6ef0e06e67271645ad92addea85559e
tran anh thy <anhthy18072003.tran@gmail.com>
2023-06-18 12:34:41 +0700

    I am testing commit code

---
+++ QuanLyThuVien/Form2.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace QuanLyThuVien
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            monthCalendar1 = new MonthCalendar();
+            SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(71, 40);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(monthCalendar1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private MonthCalendar monthCalendar1;
+    }
+}
